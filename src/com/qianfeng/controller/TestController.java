7f5bfe839101b7be38451146947dc5c09b0ba915
“me <chenzetao6666@163.com>
2019-12-08 22:54:10 +0800

    加点测试代码

--- src/com/qianfeng/controller/TestController.java
+++ src/com/qianfeng/controller/TestController.java
@@ -7,5 +7,6 @@ package com.qianfeng.controller;
 public class TestController {
     public static void main(String[] args){
         System.out.println("测试提交项目到github!!");
+        System.out.println("现在加点测试代码!!");
     }
 }
