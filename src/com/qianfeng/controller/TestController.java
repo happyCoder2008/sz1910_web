8b27630a2291ce8ca8f57aa3b0aeeb6ae01b8685
“me <chenzetao6666@163.com>
2019-12-08 22:58:04 +0800

    设置了忽略文件

--- src/com/qianfeng/controller/TestController.java
+++ src/com/qianfeng/controller/TestController.java
@@ -8,5 +8,6 @@ public class TestController {
     public static void main(String[] args){
         System.out.println("测试提交项目到github!!");
         System.out.println("现在加点测试代码!!");
+        System.out.println("设置了忽略文件");
     }
 }
